fc448ec8bbd87fcb2287001eafdb0e1adb82e275
Frost Ming <mianghong@gmail.com>
2017-11-08 17:15:22 -0800

    Support single sided space insertion (#20)
    * Add pangu contrib
    
    * Support single sided space insertion.

--- mistune_contrib/pangu.py
+++ mistune_contrib/pangu.py
@@ -1,3 +1,4 @@
+# -*- coding:utf-8 -*-
 """
     mistune_contrib.pangu
     ~~~~~~~~~~~~~~~~~~~~~
@@ -18,8 +19,10 @@ else:
         return s
 CJK_RE = (r'\u2e80-\u2eff\u2f00-\u2fdf\u3040-\u309f\u30a0-\u30ff\u3100-\u312f'
           r'\u3200-\u32ff\u3400-\u4dbf\u4e00-\u9fff\uf900-\ufaff')
-PANGU_RE = re.compile(u(r'(?<=[%s])([^%s]+)(?=[%s])' %
-                        (CJK_RE, CJK_RE, CJK_RE)))
+PANGU_RE = re.compile(
+    u(r'((?<=[{cjk}])[ \t]*(?=[^\n{cjk}])|(?<=[^\n{cjk}])[ \t]*(?=[{cjk}]))'
+      .format(cjk=CJK_RE))
+)
 
 
 class PanguRendererMixin(object):
@@ -33,11 +36,11 @@ class PanguRendererMixin(object):
     :Example:
 
         input: 中国有13亿人口
-        output: 中国有<span class="pangu">13</span>亿人口
+        output: 中国有<span class="pangu"></span>13<span class="pangu"></span>亿人口
 
     From: `vinta's pangu project <https://github.com/vinta/pangu.js>`_
     """
     def text(self, text):
         rv = super(PanguRendererMixin, self).text(text)
-        rv = PANGU_RE.sub(r'<span class="pangu">\1</span>', rv)
+        rv = PANGU_RE.sub('<span class="pangu"></span>', rv)
         return rv
